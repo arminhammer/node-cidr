572bdf34ebe6d5bbb938f6af702c589a96f89aed
Armin Graf <arminhammer@gmail.com>
2017-03-02 10:46:39 -0500

    Removing ignored dirs

--- node_modules/@ava/babel-preset-transform-test-files/package-hash.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const packageHash = require('package-hash');
-
-module.exports = packageHash.sync([
-	require.resolve('./package.json'),
-	require.resolve('babel-plugin-espower/package.json'),
-	require.resolve('babel-plugin-ava-throws-helper/package.json')
-]);
